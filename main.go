6fddc1575af38f218a7d2e02fd3f45e10a8e08aa
Adam Gustafsson <adam.gust@hotmail.se>
2024-07-30 23:42:45 +0200

    (wip) begin ast and parser

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"dodo-lang/repl"
+	"fmt"
+	"os"
+	"os/user"
+)
 
+func main() {
+	user, err := user.Current()
+
+	if err != nil {
+		panic(err)
+	}
+
+	// REPL
+	fmt.Printf("\nHello %s! You are now running the Dodo programming language.\n", user.Username)
+	fmt.Printf("Now, type some commands below :)\n\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
